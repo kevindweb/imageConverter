da03af944c47f61741705311f90648986cddf8f0
Kevin Deems <kevin8deems@gmail.com>
2022-01-23 11:08:35 -0500

    Optimized the algorithm and added documentation

---
+++ README.md
@@ -0,0 +1,36 @@
+# ImageConverter
+
+## Description
+* The goal is to take a jpeg image and convert it to png
+* It strips the "background" away to make it a transparent icon
+* There is no user input, the program determines the background automatically
+
+## Example
+We want to input this jpeg image  
+![Regular AWS lambda jpeg](/images/lambda.jpeg)  
+
+
+And turn it into an icon with a transparent background like this  
+![Exciting icon without background](/icons/lambda.png)  
+
+
+## Running the program
+* Install golang and execute `go build && ./main`
+    * The default image is "clownfish" 
+    * You can run another in the images folder with `go run converter.go <image name>`
+        * Example: `go run converter.go cloudformation`
+        * The output will go into "icons"
+
+## Key algorithms
+* Find background color - n^2 image lookup with hash table for the color:pixelCount
+    * Find the most "popular" color (set as the background color)
+* Find connected components - n^2 dfs to find separate connected components/potential icons
+    * We loop through pixels and group them by neighbors, making sure to `visit` them only once
+    * This process also "fits" the image by finding the component's dimensions
+* Build transparent image - n^2 only add color of pixels if they are in the correct "component"
+
+(where n^2 is the total number of pixels - width*height)
+
+## Key points and features
+* The algorithm performs especially well in jpegs that "should" be icons with semi-uniform background colors
+* The output png has its' dimensions fixed to the icon to reduce the photo size
--- converter.go
+++ converter.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"image"
 	"image/color"
-	"image/draw"
 	"image/jpeg"
 	"image/png"
 	"io/ioutil"
@@ -23,64 +21,58 @@ func init() {
 	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
 }
 
-func maxUpdate(highestColor *color.Color, contenderColor color.Color, colorMap map[color.Color]int) {
-	if colorMap[contenderColor] > colorMap[*highestColor] {
-		*highestColor = contenderColor
-	}
+func readFile(fileToConvert string) image.Image {
+	file, err := os.Open("images/" + fileToConvert + ".jpeg")
+	check(err)
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	check(err)
+
+	return img
 }
 
-func findBackgroundColor(img image.Image, width int, height int) color.Color {
+func writeFile(fileName string, background *image.RGBA) {
+	buf := new(bytes.Buffer)
+	err := png.Encode(buf, background)
+	check(err)
+
+	err = ioutil.WriteFile("icons/"+fileName+".png", buf.Bytes(), 0644)
+	check(err)
+}
+
+func findBackgroundColor(img image.Image, width int, height int) [3]uint32 {
 	colorCount := make(map[color.Color]int)
 	popularColor := img.At(0, 0)
 	colorCount[popularColor] = 1
+	maxPixelCount := 1
 
 	for x := 1; x < width; x++ {
 		for y := 0; y < height; y++ {
 			pixel := img.At(x, y)
+			pixelCount := 1
 			if count, ok := colorCount[pixel]; ok {
-				colorCount[pixel] = count + 1
-			} else {
-				colorCount[pixel] = 1
+				pixelCount += count
 			}
-
-			maxUpdate(&popularColor, pixel, colorCount)
+			if pixelCount > maxPixelCount {
+				popularColor = pixel
+				maxPixelCount = pixelCount
+			}
+			colorCount[pixel] = pixelCount
 		}
 	}
 
-	return popularColor
+	red, green, blue, _ := popularColor.RGBA()
+	return [3]uint32{red, green, blue}
 }
 
 func square(num uint32) float64 {
 	return float64(num * num)
 }
 
-func colorDiff(c1 color.Color, c2 color.Color) float64 {
-	r2, g2, b2, _ := c2.RGBA()
+func colorDiff(c1 color.Color, background [3]uint32) float64 {
 	r1, g1, b1, _ := c1.RGBA()
-	diff := math.Sqrt(square(r2-r1) + square(g2-g1) + square(b2-b1))
-	return diff
-}
-
-func transparentImage(width int, height int) *image.RGBA {
-	transparentColor := image.Transparent
-	background := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(background, background.Bounds(), transparentColor, image.Point{0, 0}, draw.Src)
-	return background
-}
-
-func readFile() (image.Image, string) {
-	fileToConvert := "cloudformation"
-	if len(os.Args) >= 2 {
-		fileToConvert = os.Args[1]
-	}
-
-	file, err := os.Open(fileToConvert + ".jpeg")
-	check(err)
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	check(err)
-
-	return img, fileToConvert
+	return math.Sqrt(square(background[0]-r1) +
+		square(background[1]-g1) + square(background[2]-b1))
 }
 
 type componentPixel struct {
@@ -95,13 +87,18 @@ func (c *componentPixel) visit(pixel color.Color, component int) {
 	c.visited = true
 }
 
-func dfs(col int, row int, width int, height int, matrix []componentPixel, img image.Image, background color.Color, component int) int {
+func dfs(col int, row int, width int, height int, matrix []componentPixel,
+	img image.Image, background [3]uint32, component int) (int, [4]int) {
 	var stack [][2]int
 	stack = append(stack, [2]int{col, row})
 	var col_row [2]int
 
 	count := 0
 	neighbors := [3]int{-1, 0, 1}
+
+	firstPixel := true
+	var pixelSpace [4]int
+
 	for len(stack) > 0 {
 		col_row, stack = stack[len(stack)-1], stack[:len(stack)-1]
 		col = col_row[0]
@@ -111,89 +108,136 @@ func dfs(col int, row int, width int, height int, matrix []componentPixel, img i
 			continue
 		}
 
-		if matrix[row*width+col].visited {
+		inx := row*width + col
+		if matrix[inx].visited {
 			continue
 		}
+
 		pixel := img.At(col, row)
-		if colorDiff(pixel, background) < 10000 {
+		if colorDiff(pixel, background) < 15000 {
 			// this is the background, don't want this component
 			continue
 		}
 
-		matrix[row*width+col].visit(pixel, component)
+		if firstPixel {
+			// initialize the dimensions only when necessary
+			firstPixel = false
+			const MaxInt = int(^uint(0) >> 1)
+			pixelSpace = [4]int{MaxInt, 0, MaxInt, 0}
+		}
+
+		// update dimensions of this icon [top, bottom, left, right]
+		if row < pixelSpace[0] {
+			pixelSpace[0] = row
+		} else if row > pixelSpace[1] {
+			pixelSpace[1] = row
+		}
+		if col < pixelSpace[2] {
+			pixelSpace[2] = col
+		} else if col > pixelSpace[3] {
+			pixelSpace[3] = col
+		}
+
+		matrix[inx].visit(pixel, component)
 
 		for i := 0; i < len(neighbors); i++ {
+			neighborColumn := col + neighbors[i]
 			for j := 0; j < len(neighbors); j++ {
-				stack = append(stack, [2]int{col + neighbors[i], row + neighbors[j]})
+				stack = append(stack, [2]int{neighborColumn, row + neighbors[j]})
 			}
 		}
 
 		count += 1
 	}
 
-	return count
+	return count, pixelSpace
 }
 
-func findIcon(width int, height int, background color.Color, img image.Image) ([]componentPixel, int) {
+func findIcon(width int, height int,
+	background [3]uint32, img image.Image) ([]componentPixel, int, [4]int) {
 	/*
-	 * find connected components
-	 * find connected component with highest numPixels
-	 * only add that component to the transparent list
-	 * may be able to create a more efficient "adjcency list" instead of 2d matrix
-	 	* loop over and draw only the actual component instead of "searching" for a non-transparent pixel
+		 * find connected components
+		 	* connected components are surrounded by "background" color
+			* which separates them from other components
+		 * find connected component with highest numPixels
+		 * trim the image into only the icon's dimensions to save space
 	*/
 	matrix := make([]componentPixel, width*height)
 
 	components := 0
 	maxComponent := 0
 	maxComponentPixelCount := 0
+	var componentPixelCount int
 
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
-			componentPixelCount := dfs(i, j, width, height, matrix, img, background, components)
+	var componentDimensions [][4]int
+	var dimensions [4]int
+
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			componentPixelCount, dimensions = dfs(i, j, width, height, matrix, img,
+				background, components)
 			if componentPixelCount > 0 {
 				if componentPixelCount > maxComponentPixelCount {
 					maxComponentPixelCount = componentPixelCount
 					maxComponent = components
 				}
+
+				componentDimensions = append(componentDimensions, dimensions)
 				components += 1
 			}
 		}
 	}
 
-	fmt.Println("Iconcomponent Pixelcount", maxComponentPixelCount)
-	return matrix, maxComponent
+	return matrix, maxComponent, componentDimensions[maxComponent]
 }
 
-/*
-features
-* make width and height only the actual icon instead of original photo
-* cli argument for the 10000 number, find a name for this
-*/
-
-func main() {
-	img, fileName := readFile()
-
+func runIcon(img image.Image) *image.RGBA {
 	backgroundWidth := img.Bounds().Dx()
 	backgroundHeight := img.Bounds().Dy()
+	transparentColor := image.Transparent
+
 	backgroundColor := findBackgroundColor(img, backgroundWidth, backgroundHeight)
-	matrix, iconComponent := findIcon(backgroundWidth, backgroundHeight, backgroundColor, img)
+	matrix, iconComponent, iconDimensions := findIcon(backgroundWidth,
+		backgroundHeight, backgroundColor, img)
 
-	background := transparentImage(backgroundWidth, backgroundHeight)
+	topPixel := iconDimensions[0]
+	bottomPixel := iconDimensions[1]
+	leftPixel := iconDimensions[2]
+	rightPixel := iconDimensions[3]
 
-	for i := 0; i < backgroundWidth; i++ {
-		for j := 0; j < backgroundHeight; j++ {
-			pixel := matrix[j*backgroundWidth+i]
+	iconWidth := rightPixel - leftPixel
+	iconHeight := bottomPixel - topPixel
+	background := image.NewRGBA(image.Rect(0, 0, iconWidth, iconHeight))
+	matrixInx := topPixel*backgroundWidth + leftPixel
+	var row int
+
+	for j := 0; j < iconHeight; j++ {
+		row = j*backgroundWidth + matrixInx
+		for i := 0; i < iconWidth; i++ {
+			// original matrix index is (j+topPixel)*backgroundWidth+(i+leftPixel)
+			// accessing 2d matrix as 1d array https://stackoverflow.com/a/2151141
+			pixel := matrix[row+i]
 			if pixel.pixel != nil && pixel.component == iconComponent {
 				background.Set(i, j, pixel.pixel)
+			} else {
+				// just make it transparent
+				background.Set(i, j, transparentColor)
 			}
 		}
 	}
 
-	buf := new(bytes.Buffer)
-	err := png.Encode(buf, background)
-	check(err)
+	return background
+}
 
-	err = ioutil.WriteFile(fileName+".png", buf.Bytes(), 0644)
-	check(err)
+func main() {
+	fileName := "clownfish"
+	if len(os.Args) >= 2 {
+		fileName = os.Args[1]
+	}
+
+	img := readFile(fileName)
+
+	background := runIcon(img)
+
+	writeFile(fileName, background)
 }
